f13389f49476db539553fd6e646c552795bd1415
LeeHongSoek <lhs0806@gmail.com>
2023-12-18 21:25:16 +0900

    postman 하고sql로깅..

--- gs-spring-boot-complete/src/main/java/com/example/www/controller/MemberController.java
+++ gs-spring-boot-complete/src/main/java/com/example/www/controller/MemberController.java
@@ -2,6 +2,8 @@ package com.example.www.controller;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.www.ClassUtils;
 import com.example.www.service.MemberService;
 import com.example.www.vo.Member;
 
@@ -18,6 +21,7 @@ import com.example.www.vo.Member;
 @RequestMapping("/api/members")
 public class MemberController
 {
+    private final Logger logger = LogManager.getLogger(getClass().getName());
     private final MemberService memberService;
 
     public MemberController(MemberService memberService)
@@ -28,30 +32,45 @@ public class MemberController
     @GetMapping
     public List<Member> getAllMembers()
     {
+        logger.info("◆ 클래스: {} ", ClassUtils.getShtClassNm(getClass()));
+        logger.info("◇ {} ", "@GetMapping");
+        
         return memberService.getAllMembers();
     }
 
     @GetMapping("/{id}")
     public Member getMemberById(@PathVariable Long id)
     {
+        logger.info("◆ 클래스: {} ", ClassUtils.getShtClassNm(getClass()));
+        logger.info("◇ {} ", "@GetMapping('/{id}')");
+        
         return memberService.getMemberById(id);
     }
 
     @PostMapping
     public Member createMember(@RequestBody Member member)
     {
+        logger.info("◆ 클래스: {} ", ClassUtils.getShtClassNm(getClass()));
+        logger.info("◇ {} ", "@PostMapping");
+        
         return memberService.createMember(member);
     }
 
     @PutMapping("/{id}")
     public Member updateMember(@PathVariable Long id, @RequestBody Member member)
     {
+        logger.info("◆ 클래스: {} ", ClassUtils.getShtClassNm(getClass()));
+        logger.info("◇ {} ", "@PutMapping('/{id}')");
+        
         return memberService.updateMember(id, member);
     }
 
     @DeleteMapping("/{id}")
     public void deleteMember(@PathVariable Long id)
     {
+        logger.info("◆ 클래스: {} ", ClassUtils.getShtClassNm(getClass()));
+        logger.info("◇ {} ", "@PutMapping('/{id}')");
+
         memberService.deleteMember(id);
     }
 }
--- gs-spring-boot-complete/src/main/java/com/example/www/controller/_RootController.java
+++ gs-spring-boot-complete/src/main/java/com/example/www/controller/_RootController.java
@@ -2,24 +2,23 @@ package com.example.www.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.www.ClassUtils;
 
-@RestController
+@Controller
 public class _RootController
 {
-	
-	private final Logger logger = LogManager.getLogger(getClass().getName());
+
+    private final Logger logger = LogManager.getLogger(getClass().getName());
 
     @GetMapping("/")
     public String index()
     {
-    	logger.info("◆ 클래스: {} ", ClassUtils.getShtClassNm(getClass()));
-    	
-    	
-        return "Root!!<a href='/showendpoints'>/showendpoints</a>";
+        logger.info("◆ 클래스: {} ", ClassUtils.getShtClassNm(getClass()));
+
+        return "_root";
     }
 
 }
