33fbc824422154b24b7c9162c7d4878cea983752
이홍석(GMail) <lhs0806@gmail.com>
2023-12-29 19:30:26 +0900

    [Endpoint 리스트]

--- gs-spring-boot-complete/src/main/java/com/example/www/controller/ShowInfoController.java
+++ gs-spring-boot-complete/src/main/java/com/example/www/controller/ShowInfoController.java
@@ -3,7 +3,6 @@ package com.example.www.controller;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -25,177 +24,139 @@ import com.example.www.vo.BeanInfo;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+ 
+
 @Controller
 public class ShowInfoController
 {
 
-    private final Logger logger = LogManager.getLogger(getClass().getName());
-    private final RequestMappingHandlerMapping handlerMapping;
+	private final Logger logger = LogManager.getLogger(getClass().getName());
+	private final RequestMappingHandlerMapping handlerMapping;
 
-    public ShowInfoController(@Qualifier("requestMappingHandlerMapping") RequestMappingHandlerMapping handlerMapping)
-    {
-        this.handlerMapping = handlerMapping;
-    }
-/*
-    private String formatHandlerMethod(String handlerMethod)
-    {
-        // 클래스명과 함수명 사이의 마지막 '.'를 ':', 괄호 내부의 '.'를 '/'로 대체
-        // handlerMethod = handlerMethod.replaceAll("\\.(?!.*\\))", " : ").replaceAll("\\.(?=[^()]*\\))", " / ");
-        return handlerMethod;
-    }
-  */  
-    private String formatHandlerMethod(String handlerMethod) {
-        // 1. 문장을 분리
-        String[] parts = handlerMethod.split("\\(");
-        String methodSignature = parts[0];
-        String packageSignature = null;
-        String clseeSignature = null;
-        String parameters = parts[1].substring(0, parts[1].lastIndexOf(")"));
-        String[] parameterArray = parameters.split(", ");
-
-        // 2. 클래스명과 함수명 변환
-        int lastDotIndex = methodSignature.lastIndexOf(".");
-        if (lastDotIndex != -1) {
-            methodSignature = methodSignature.substring(0, lastDotIndex) + " : " + methodSignature.substring(lastDotIndex + 1);
-            
-            lastDotIndex = methodSignature.substring(0, lastDotIndex).lastIndexOf(".");
-            if (lastDotIndex != -1) {
-                methodSignature = methodSignature.substring(0, lastDotIndex) + " : " + methodSignature.substring(lastDotIndex + 1);
-
-                packageSignature = null;
-                clseeSignature = null;
-            }
-        }
-
-        // 3. 각 파라미터 변환
-        for (int i = 0; i < parameterArray.length; i++) {
-            int lastDotIndexParam = parameterArray[i].lastIndexOf(".");
-            if (lastDotIndexParam != -1) {
-                parameterArray[i] = parameterArray[i].substring(0, lastDotIndexParam) + " / " + parameterArray[i].substring(lastDotIndexParam + 1);
-            }
-        }
-
-        // 4. 결과 문자열 조합
-        String formattedMethod = methodSignature + "(" + String.join(", ", parameterArray) + ")";
-        return formattedMethod;
-    }
+	public ShowInfoController(@Qualifier("requestMappingHandlerMapping") RequestMappingHandlerMapping handlerMapping)
+	{
+		this.handlerMapping = handlerMapping;
+	}
 
 
-    @GetMapping("/showendpoints")
-    public ModelAndView showEndpoints(Model model, HttpServletRequest request)
-    {
-        logger.info("◇ 클래스:함수명 | {}:{} ", ClassUtils.getShtClassNm(getClass()), Thread.currentThread().getStackTrace()[1].getMethodName());
+	@GetMapping("/showendpoints")
+	public ModelAndView showEndpoints(Model model, HttpServletRequest request)
+	{
+		logger.info("◇ 클래스:함수명 | {}:{} ", ClassUtils.getShtClassNm(getClass()), Thread.currentThread().getStackTrace()[1].getMethodName());
 
-        // 동적으로 현재 세션에 걸려있는 도메인 주소 가져오기
-        String baseDomain = request.getServerName() + ":" + request.getServerPort();
-        model.addAttribute("baseDomain", baseDomain);
+		// 동적으로 현재 세션에 걸려있는 도메인 주소 가져오기
+		String baseDomain = request.getServerName() + ":" + request.getServerPort();
+		model.addAttribute("baseDomain", baseDomain);
 
-        List<String[]> endpointList = new ArrayList<>();
+		List<String[]> endpointList = new ArrayList<>();
 
-        // handlerMapping을 사용하여 endpointList 채우기
-        handlerMapping.getHandlerMethods().forEach((key, value) ->
-        {
-            String strKey = key.getPatternValues().toString().replaceAll("\\[|\\]", "");
-            String strValue = value.getMethod().toString();
-            String requestMethod = key.getMethodsCondition().getMethods().toString();
+		// handlerMapping을 사용하여 endpointList 채우기
+		handlerMapping.getHandlerMethods().forEach((key, value) ->
+		{
+			String strKey = key.getPatternValues().toString().replaceAll("\\[|\\]", "");
+			
+			String strValue = value.getMethod().toString();
+			String requestMethod = key.getMethodsCondition().getMethods().toString();
+			
+			MethodInfo parsedInfo = parseMethodSignature(strValue);
+			
+			if ("com.example.www.controller".equals(parsedInfo.packageName))
+			{
+	            String[] endpointArray = { requestMethod
+	                                     , strKey
+	                                     //, parsedInfo.accessModifier
+	                                     //, parsedInfo.packageName
+	                                     , parsedInfo.className
+	                                     , parsedInfo.returnType
+	                                     , parsedInfo.methodName
+	                                     , parsedInfo.param
+	                                     };
+				endpointList.add(endpointArray);
+			}
+		});
 
-            // Create an array for each endpoint
-            String[] endpointArray =
-            { strKey, requestMethod, formatHandlerMethod(strValue) };
-            endpointList.add(endpointArray);
-        });
+		Collections.sort(endpointList, Comparator.comparing(endpointArray -> endpointArray[1]));
 
-        // Sort the list based on the endpoint pattern (strKey)
-        Collections.sort(endpointList, Comparator.comparing(endpointArray -> endpointArray[0]));
+		endpointList.forEach(endpointArray -> logger.info("◇ {} : {}, {}: {}", endpointArray[0], endpointArray[1], endpointArray[2]));
 
-        // Log and print the sorted entries
-        endpointList.forEach(endpointArray -> logger.info("◇ {} : {}, {}: {}", endpointArray[0], endpointArray[1], endpointArray[2]));
+		// Add sorted entries to the model
+		model.addAttribute("endpointList", endpointList);
 
-        // Add sorted entries to the model
-        model.addAttribute("endpointList", endpointList);
+		return new ModelAndView("showendpoints");
+	}
 
-        return new ModelAndView("showendpoints");
-    }
+	class MethodInfo
+	{
+		public String accessModifier;
+		public String returnType;
+		
+		public String methodFullName;
+		public String packageName;
+		public String className;
+		public String methodName;
+		public String param;
+	}
 
-    @GetMapping("/showendpoints_")
-    public ModelAndView showEndpoints_(Model model, HttpServletRequest request)
-    {
-        logger.info("◇ 클래스:함수명 | {}:{} ", ClassUtils.getShtClassNm(getClass()), Thread.currentThread().getStackTrace()[1].getMethodName());
+	private MethodInfo parseMethodSignature(String methodSignature)
+	{
+		MethodInfo methodInfo = new MethodInfo();
 
-        // 동적으로 현재 세션에 걸려있는 도메인 주소 가져오기
-        String baseDomain = request.getServerName() + ":" + request.getServerPort();
-        model.addAttribute("baseDomain", baseDomain);
+		String[] parts = methodSignature.split(" ");
+		
+		methodInfo.accessModifier = parts[0];
+		methodInfo.returnType = parts[1];
+        String methodName = parts[2];        
 
-        Map<String, List<String>> endpointMap = new HashMap<>();
+        int lastParenthesisIndex1 = methodName.lastIndexOf("(");
+        int lastParenthesisIndex2 = methodName.lastIndexOf(")");
+        int lastDotIndex = methodName.lastIndexOf(".", lastParenthesisIndex1);
 
-        // handlerMapping을 사용하여 endpointMap 채우기
-        handlerMapping.getHandlerMethods().forEach((key, value) ->
-        {
-            String strKey = key.getPatternValues().toString().replaceAll("\\[|\\]", "");
-            String strValue = value.getMethod().toString();
-            String requestMethod = key.getMethodsCondition().getMethods().toString();
+        methodInfo.methodFullName = methodName;
+        methodInfo.methodName = methodName.substring(lastDotIndex + 1, lastParenthesisIndex1);
+        methodInfo.className = methodName.substring(methodName.lastIndexOf(".", lastDotIndex - 1) + 1, lastDotIndex);
+        methodInfo.packageName = methodName.substring(0, methodName.lastIndexOf(methodInfo.className)-1);
+        methodInfo.param = methodName.substring(lastParenthesisIndex1, lastParenthesisIndex2+1);
 
-            // Create a list for each endpoint containing the method and value
-            List<String> methodList = endpointMap.computeIfAbsent(strKey, k -> new ArrayList<>());
-            methodList.add(requestMethod + ": " + strValue);
-        });
+		return methodInfo;
+	}
 
-        // Transform the map into a list of entries
-        List<Map.Entry<String, List<String>>> sortedEntries = new ArrayList<>(endpointMap.entrySet());
 
-        // Sort the entries based on the endpoint key
-        Collections.sort(sortedEntries, Comparator.comparing(Map.Entry::getKey));
+	
+	@Autowired
+	private BeansEndpoint beansEndpoint;
 
-        // Log and print the sorted entries
-        sortedEntries.forEach(entry ->
-        {
-            String strKey = entry.getKey();
-            List<String> methodList = entry.getValue();
+	@GetMapping("/showbeans")
+	public String showBeans(Model model)
+	{
+		logger.info("◇ 클래스:함수명 | {}:{} ", ClassUtils.getShtClassNm(getClass()), Thread.currentThread().getStackTrace()[1].getMethodName());
 
-            logger.info("◇ {} : {}", strKey, methodList);
-        });
+		BeansDescriptor beansDescriptor = beansEndpoint.beans();
 
-        // Add sorted entries to the model
-        model.addAttribute("endpointMap", sortedEntries);
+		List<BeanInfo> beanInfoList = new ArrayList<>();
 
-        return new ModelAndView("showendpoints");
-    }
+		String basePackage = this.getClass().getPackage().getName();
 
-    @Autowired
-    private BeansEndpoint beansEndpoint;
+		for (Map.Entry<String, ContextBeansDescriptor> contextEntry : beansDescriptor.getContexts().entrySet())
+		{
+			String context = contextEntry.getKey();
+			ContextBeansDescriptor contextBeansDescriptor = contextEntry.getValue();
 
-    @GetMapping("/showbeans")
-    public String showBeans(Model model)
-    {
-        logger.info("◇ 클래스:함수명 | {}:{} ", ClassUtils.getShtClassNm(getClass()), Thread.currentThread().getStackTrace()[1].getMethodName());
+			for (Map.Entry<String, BeansEndpoint.BeanDescriptor> beanEntry : contextBeansDescriptor.getBeans().entrySet())
+			{
+				String beanName = beanEntry.getKey();
+				String beanClass = beanEntry.getValue().getType().getName();
 
-        BeansDescriptor beansDescriptor = beansEndpoint.beans();
+				// 패키지가 현재 클래스가 속한 패키지로 시작하는 빈들만 추가
+				if (beanClass.startsWith(basePackage))
+				{
+					String modifiedBeanClass = beanClass.replace(basePackage + ".", "");
+					BeanInfo beanInfo = new BeanInfo(context, beanName, modifiedBeanClass);
+					beanInfoList.add(beanInfo);
+				}
+			}
+		}
 
-        List<BeanInfo> beanInfoList = new ArrayList<>();
-
-        String basePackage = this.getClass().getPackage().getName();
-
-        for (Map.Entry<String, ContextBeansDescriptor> contextEntry : beansDescriptor.getContexts().entrySet())
-        {
-            String context = contextEntry.getKey();
-            ContextBeansDescriptor contextBeansDescriptor = contextEntry.getValue();
-
-            for (Map.Entry<String, BeansEndpoint.BeanDescriptor> beanEntry : contextBeansDescriptor.getBeans().entrySet())
-            {
-                String beanName = beanEntry.getKey();
-                String beanClass = beanEntry.getValue().getType().getName();
-
-                // 패키지가 현재 클래스가 속한 패키지로 시작하는 빈들만 추가
-                if (beanClass.startsWith(basePackage))
-                {
-                    String modifiedBeanClass = beanClass.replace(basePackage + ".", "");
-                    BeanInfo beanInfo = new BeanInfo(context, beanName, modifiedBeanClass);
-                    beanInfoList.add(beanInfo);
-                }
-            }
-        }
-
-        model.addAttribute("beanInfoList", beanInfoList);
-        return "showbeans";
-    }
+		model.addAttribute("beanInfoList", beanInfoList);
+		return "showbeans";
+	}
 }
